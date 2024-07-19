d32617bb77453a23718584ae04e92546d7f8a6ac
Shashank Prajapati <shashank.prajapati@gosamplify.com>
2024-07-19 21:06:01 +0530

    Change

--- src/app/workout-form/workout-form.component.ts
+++ src/app/workout-form/workout-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WorkoutService } from '../workout.service';
 import { User } from '../models/user';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-workout-form',
@@ -10,30 +10,32 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./workout-form.component.scss']
 })
 export class WorkoutFormComponent implements OnInit {
-  workoutForm: FormGroup;
+  workoutForm!: FormGroup;
   workoutTypes: string[] = ['Running', 'Swimming', 'Cycling', 'Yoga', 'Strength Training'];
   isEdit: boolean = false;
+  userId!: number;
 
   constructor(
     private fb: FormBuilder,
     private workoutService: WorkoutService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
+  ) {}
+
+  ngOnInit() {
     this.workoutForm = this.fb.group({
       name: ['', Validators.required],
       type: ['', Validators.required],
       minutes: ['', [Validators.required, Validators.min(1)]]
     });
-  }
 
-  ngOnInit() {
     this.route.queryParams.subscribe(params => {
       const id = params['id'];
       if (id) {
         this.workoutService.users$.subscribe(users => {
           const userToEdit = users.find((user: User) => user.id === +id);
           if (userToEdit) {
+            this.userId = userToEdit.id;
             this.workoutForm.patchValue(userToEdit);
             this.isEdit = true;
           }
@@ -43,18 +45,16 @@ export class WorkoutFormComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.workoutForm.invalid) {
-      return;
+    if (this.workoutForm.valid) {
+      const formValue = this.workoutForm.value;
+      if (this.isEdit) {
+        const updatedUser: User = { id: this.userId, ...formValue };
+        this.workoutService.updateUser(updatedUser);
+      } else {
+        const newUser: User = { id: Date.now(), ...formValue };
+        this.workoutService.addUser(newUser);
+      }
+      this.router.navigate(['/list']);
     }
-
-    const user = this.workoutForm.value;
-
-    if (this.isEdit) {
-      this.workoutService.updateUser(user);
-    } else {
-      const newUser = { ...user, id: Date.now() };
-      this.workoutService.addUser(newUser);
-    }
-    this.router.navigate(['/list']);
   }
 }
